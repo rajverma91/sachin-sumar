64137f7934c95d2e8ace5e6b56abfa06bb74518f
rajverma91 <66714037+rajverma91@users.noreply.github.com>
2025-04-19 05:04:00 +0000

    update workm

--- src/pages/light/home-creative-agency/index.jsx
+++ src/pages/light/home-creative-agency/index.jsx
@@ -17,7 +17,7 @@ import Team from '@/components/CreativeAgency/Team';
 import HzScroll from '@/components/CreativeAgency/HorizontalScroll';
 import Blog from '@/components/CreativeAgency/Blog';
 import Footer from '@/components/CreativeAgency/Footer';
-import Works from '@/components/CreativeAgency/HorizontalScroll/Worksm';
+
 
 function HomeCreativeAgencyLight() {
   useEffect(() => {
